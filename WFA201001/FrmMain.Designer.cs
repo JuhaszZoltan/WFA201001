69aa3e6975c3a92bc1218963b92d2144b2fb0eb7
Juhasz Zoltan <juhaszzoltanmate@yahoo.com>
2020-10-01 19:26:35 +0200

    Add project files.

---
+++ WFA201001/FrmMain.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace WFA201001
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgvNevek = new System.Windows.Forms.DataGridView();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nev = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.tbNevInput = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNevek)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvNevek
+            // 
+            this.dgvNevek.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvNevek.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvNevek.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvNevek.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.Nev});
+            this.dgvNevek.Location = new System.Drawing.Point(12, 12);
+            this.dgvNevek.Name = "dgvNevek";
+            this.dgvNevek.Size = new System.Drawing.Size(614, 294);
+            this.dgvNevek.TabIndex = 0;
+            // 
+            // Id
+            // 
+            this.Id.HeaderText = "Id";
+            this.Id.Name = "Id";
+            // 
+            // Nev
+            // 
+            this.Nev.FillWeight = 200F;
+            this.Nev.HeaderText = "Név";
+            this.Nev.Name = "Nev";
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(12, 321);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(159, 66);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "Betöltés";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.BtnLoad_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(177, 347);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(159, 40);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Hozzáad";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // tbNevInput
+            // 
+            this.tbNevInput.Location = new System.Drawing.Point(177, 321);
+            this.tbNevInput.Name = "tbNevInput";
+            this.tbNevInput.Size = new System.Drawing.Size(159, 20);
+            this.tbNevInput.TabIndex = 2;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 399);
+            this.Controls.Add(this.tbNevInput);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.dgvNevek);
+            this.Name = "FrmMain";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNevek)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvNevek;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nev;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox tbNevInput;
+    }
+}
+
---
+++ WFA201001/FrmMain.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA201001
+{
+    public partial class FrmMain : Form
+    {
+        SqlConnection conn;
+
+        public FrmMain()
+        {
+            conn = new SqlConnection(
+                @"Data Source=(localdb)\MSSQLLocalDB;" +
+                "Initial Catalog=proba;");
+
+            var list = new List<string>()
+            {
+                "dfgtdfh",
+                "gfsdrghdtrgh",
+            };
+
+
+            InitializeComponent();
+        }
+
+        private void BtnLoad_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+
+            var cmd = new SqlCommand("SELECT * FROM emberek;", conn);
+            var r = cmd.ExecuteReader();
+
+            while (r.Read())
+            {
+                dgvNevek.Rows.Add(r[0], r[1]);
+            }
+            conn.Close();
+        }
+
+        private void BtnAdd_Click(object sender, EventArgs e)
+        {
+            conn.Open();
+            var adapter = new SqlDataAdapter()
+            {
+                InsertCommand = new SqlCommand(
+                $"INSERT INTO emberek (nev) VALUES ('{tbNevInput.Text}');", conn),
+            };
+            adapter.InsertCommand.ExecuteNonQuery();
+            conn.Close();
+        }
+    }
+}
